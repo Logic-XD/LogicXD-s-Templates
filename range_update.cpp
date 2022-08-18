6c3c66d1f622a70a1b165cf3b2aa9cd055d89995
Logic-XD <106361649+Logic-XD@users.noreply.github.com>
2022-08-18 11:15:44 -0400

    Create range_update.cpp

---
+++ range_update.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+//-----------------------
+
+vector<int> vec(100006, 0), dif(100007, 0);
+
+void update(vector<int>& D, int l, int r, int x){
+    D[l] += x; D[r + 1] -= x;
+}
+
+int main(){
+	
+	//get vec back
+	for(int i=0; i<vec.size(); i++){
+        if(i == 0) vec[i] = dif[i];
+        else vec[i] = dif[i] + vec[i - 1];
+	}
+
+	return 0;
+}
