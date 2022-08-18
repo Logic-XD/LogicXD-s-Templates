093841349465462ffe8ce3797ddc824e53c694a8
Logic-XD <106361649+Logic-XD@users.noreply.github.com>
2022-08-18 11:06:25 -0400

    Create primacy.cpp

---
+++ primacy.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+//-----------------------
+
+vector<int> p(10000007);
+
+int main(){
+  
+  for(int i=2; i<10000007; i++){
+    if(p[i]) continue;
+    for(int j=i; j<10000007; j+=i) p[j]++;
+  }
+	
+    	
+	return 0;
+}
