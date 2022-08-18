ae00e168bd16bfd291f99d6b3e3adcbbdc9b4747
Logic-XD <106361649+Logic-XD@users.noreply.github.com>
2022-08-18 11:07:26 -0400

    Update primacy.cpp

--- primacy.cpp
+++ primacy.cpp
@@ -5,12 +5,10 @@ using namespace std;
 vector<int> p(10000007);
 
 int main(){
-  
-  for(int i=2; i<10000007; i++){
-    if(p[i]) continue;
-    for(int j=i; j<10000007; j+=i) p[j]++;
-  }
-	
-    	
+
+ 	for(int i=2; i<10000007; i++){
+		if(p[i]) continue;
+		for(int j=i; j<10000007; j+=i) p[j]++;
+ 	}
 	return 0;
 }
